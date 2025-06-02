66cbf481ea6d8d12efe982396fc3bd3a6037a2d1
Evan Tschuy <evantschuy@gmail.com>
2025-06-01 23:24:19 -0700

    {css,js}: fix banner & masthead display

--- public/js/script.js
+++ public/js/script.js
@@ -5,8 +5,8 @@
     checkbox.checked = false;
   }
 
-  // if we're on mobile, show mobile-specific messages
-  if (window.navigator.userAgent.toLowerCase().includes("mobi")) {
+  // if we're on not mobile, hide mobile-specific messages
+  if (!window.navigator.userAgent.toLowerCase().includes("mobi")) {
     let banner = document.querySelector("#mobile-banner");
     if (banner) banner.style.display = 'none';
     let mobileMessage = document.querySelector("#mobile-message");
