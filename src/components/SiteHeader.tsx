d8ddbb55674a10448d709997bccc481fd1346019
Evan Tschuy <evantschuy@gmail.com>
2026-07-26 14:53:43 -0700

    app: make trailhead map link more prominent

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -6,11 +6,11 @@ export function SiteHeader() {
         <a className="wordmark" href="/" aria-label="Hiking by Transit home">Hiking by Transit</a>
         <nav aria-label="Primary navigation">
           <ul className="nav-list">
+            <li><a href="/trailheads">Trailhead map</a></li>
             <li><a href="/hikes">Hikes</a></li>
             <li><a href="/places">Places</a></li>
             <li><a href="/posts">Posts</a></li>
             <li><a href="/events">Events</a></li>
-            <li><a href="/trailheads">Trailhead map</a></li>
           </ul>
         </nav>
       </div>
